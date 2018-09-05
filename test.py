3d80967652e47f9414ee3c632ff411face91ed4b
KaungHtettAung <39527057+KaungHtettAung@users.noreply.github.com>
2018-09-05 18:14:51 +0630

    Add comments for blueprints

--- test.py
+++ test.py
@@ -19,23 +19,23 @@ class Blueprint_Test(unittest.TestCase):
 	def test_contact(self):
 		response = self.app.get("/contact")
 		self.assertEqual(response.status_code,200)
-
+	#Test for bns_leader blueprint 
 	def test_bns_leader(self):
 		response = self.app.get("/bns")
 		self.assertEqual(response.status_code,301)
-
+	#Test for cdt_parties blueprint 
 	def test_cdt_parties(self):
 		response = self.app.get("/cdt")
 		self.assertEqual(response.status_code,301)
-
+	#Test for cele blueprint 
 	def test_cele(self):		
 		response = self.app.get("/cele")
 		self.assertEqual(response.status_code,301)
-
+	#Test for mb_pmt blueprint 
 	def test_mb_pmt(self):
 		response = self.app.get("/mb")
 		self.assertEqual(response.status_code,301)
-
+	#Test for modern_figures blueprint 
 	def test_modern_figures(self):
 		response = self.app.get("/modern_figures")
 		self.assertEqual(response.status_code,301)
